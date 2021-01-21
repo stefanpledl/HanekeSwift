110afa0d8529d570c305410078a3f3731a38fd57
stefanpledl <stefanpledl@gmail.com>
2021-01-21 15:33:42 +0100

    fix package resolution

--- Package.swift
+++ Package.swift
@@ -1,9 +1,16 @@
 // swift-tools-version:5.0
 import PackageDescription
 
-let package = Package(
+let package = Package(    
     name: "Haneke",
-    platforms: [.iOS("8.0"), .tvOS("9.1")],
-    products: [.library(name: "Haneke", targets: ["Haneke"])],
-    targets: [.target(name: "Haneke", path: "Haneke")]
+    products: [
+        .library(
+            name: "Haneke",
+            targets: ["Haneke"])
+    ],
+    targets: [
+        .target(
+            name: "Haneke",
+            path: "Haneke")
+    ]
 )
