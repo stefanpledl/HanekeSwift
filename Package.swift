278ace29c87b94f875f190cc3d035c7cecb3aed3
stefapledl <stefanpledl@gmail.com>
2021-01-21 15:51:03 +0100

    Package

--- Package.swift
+++ Package.swift
@@ -1,17 +1,28 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
-let package = Package(    
-    name: "Haneke",
+let package = Package(
+    name: "HanekeSwift",
     products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Haneke",
-            targets: ["Haneke"])
+            name: "HanekeSwift",
+            targets: ["HanekeSwift"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Haneke",
-            path: ""
-            publicHeadersPath: "")
+            name: "HanekeSwift",
+            dependencies: []),
+        .testTarget(
+            name: "HanekeSwiftTests",
+            dependencies: ["HanekeSwift"]),
     ]
 )
